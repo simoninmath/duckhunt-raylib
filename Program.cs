e5fb64f5cd0f1cc95b0be0bdef64f4a1c62189d6
SIMONIN Mathieu <simonin.math@gmail.com>
2025-02-27 15:21:22 +0100

    #DHRL [Back] - Updated Progam.cs class to run game

--- Program.cs
+++ Program.cs
@@ -1,4 +1,17 @@
 ﻿using Raylib_cs;
 using static Raylib_cs.Raylib;
+using System;
 
+namespace DuckHunt
+{
+  static class Program
+  {
+    // The main entry point for the application.
+    static void Main(string[] args)
+    {
+      Game game = new Game();
+      game.Run();
+    }
+  }
+}
 
